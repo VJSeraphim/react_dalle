2f25a08619c49cfa5d111f971359c5caad134c34
VJS <minsung_96@naver.com>
2023-02-19 23:57:57 +0900

    Background init

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Form, useNavigate } from 'react-router-dom'
+import { form, useNavigate } from 'react-router-dom'
 
 import { preview } from '../assets'
 import { getRandomPrompt } from '../utils'
@@ -14,11 +14,12 @@ const handleSubmit = () => {
 }
 
 const handleChange = (e) => {
-
+  setForm({ ...form, [e.target.name]: e.target.value})
 }
 
 const handleSurpriseMe = () => {
-
+  const randomPrompt = getRandomPrompt(form.prompt)
+  setForm({ ...form, prompt: randomPrompt})
 }
 
 const CreatePost = () => {
@@ -91,9 +92,22 @@ const CreatePost = () => {
           <button
             type="button"
             onClick={generateImage}
-
+            className="text-white bg-green-700 font-medium rounded-md text-sm w-full 
+            smLw-auto px-5 py-2.5 text-center"
           >
             {generatingImg ? 'Generating...' : 'Generate'}
+          </button>
+        </div>
+        <div className="mt-10">
+          <p className="mt-2 text-[#666e75] text-[14px] ">
+            Once you have created the image you want, you can share it with others in the community.
+          </p>
+          <button 
+            type="submit"
+            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm
+            w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {loading ? 'Sharing': 'Share'}
           </button>
         </div>
       </form>
