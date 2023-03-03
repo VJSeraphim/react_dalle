7d901116f225ac4b488c111df4cb67762a87b4a3
VJS <minsung_96@naver.com>
2023-03-03 23:58:46 +0900

    Debug Session

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { form, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 import { preview } from '../assets'
 import { getRandomPrompt } from '../utils'
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -17,7 +17,7 @@ const RenderCards = ({ data, title }) => {
 const Home = () => {
   const [loading, setLoading] = useState(false)
   const [allPosts, setAllPosts] = useState(null)
-  const [searchText, setSearchText] = useState(second)
+  const [searchText, setSearchText] = useState('')
 
   return (
     <section className="max-w-7xl mx-auto">
