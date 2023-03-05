60ef06c0bd151d3c436d0759f370c8867ea3ac00
VJS <minsung_96@naver.com>
2023-03-05 23:58:56 +0900

    Posting Route + form definition Debug

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -5,44 +5,6 @@ import { preview } from '../assets'
 import { getRandomPrompt } from '../utils'
 import { FormField, Loader } from '../components'
 
-const generateImage = async () => {
-  if(form.prompt) {
-    try {
-      setGeneratingImg(true)
-      const response = await fetch('http://localhost:8080/api/v1/dalle', {
-        method: 'POST',
-        headers: {
-          'Content-Type' : 'application/json',
-        },
-        body: JSON.stringify({ prompt: form.prompt })
-      })
-
-      const data = await response.json()
-      
-      setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}`})
-    } catch (error) {
-      alert(error)
-    } finally {
-      setGeneratingImg(false)
-    }
-  } else {
-    alert('Please enter a prompt.')
-  }
-}
-
-const handleSubmit = () => {
-
-}
-
-const handleChange = (e) => {
-  setForm({ ...form, [e.target.name]: e.target.value})
-}
-
-const handleSurpriseMe = () => {
-  const randomPrompt = getRandomPrompt(form.prompt)
-  setForm({ ...form, prompt: randomPrompt})
-}
-
 const CreatePost = () => {
   const navigate = useNavigate()
   const [form, setForm] = useState({
@@ -52,6 +14,44 @@ const CreatePost = () => {
   })
   const [generatingImg, setGeneratingImg] = useState(false)
   const [loading, setLoading] = useState(false)
+
+  const generateImage = async () => {
+    if(form.prompt) {
+      try {
+        setGeneratingImg(true)
+        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+          method: 'POST',
+          headers: {
+            'Content-Type' : 'application/json',
+          },
+          body: JSON.stringify({ prompt: form.prompt })
+        })
+  
+        const data = await response.json()
+        
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}`})
+      } catch (error) {
+        alert(error)
+      } finally {
+        setGeneratingImg(false)
+      }
+    } else {
+      alert('Please enter a prompt.')
+    }
+  }
+  
+  const handleSubmit = () => {
+  
+  }
+  
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value})
+  }
+
+const handleSurpriseMe = () => {
+  const randomPrompt = getRandomPrompt(form.prompt)
+  setForm({ ...form, prompt: randomPrompt})
+}
 
   return (
     <section className="max-w-7xl mx-auto">
