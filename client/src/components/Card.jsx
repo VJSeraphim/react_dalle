7b84147a14d9763de16f73e0ebfe68ce06c675d3
VJS <minsung_96@naver.com>
2023-03-07 23:59:42 +0900

    Search Functionality

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -11,6 +11,32 @@ const Card = ({ _id, name, prompt, photo }) => {
         src={photo}
         alt={prompt}
       />
+      <div className="group-hover:flex flex-col max-h-[94.5%] hidden absoulte bottom-0 left-0 right-0 bg-[#10131f] m-2 p-4 rounded-md">
+        <p className="text-white text-sm overflow-y-auto prompt">
+          {prompt}
+        </p>
+        <div className="mt-5 flex justify-between items-center gap-2">
+          <div className="flex items-center gap-2">
+            <div className="w-7 h-7 flex justify-center items-center rounded-full object-cover bg-green-700 text-white text-xs font-bold">
+              {name[0]}
+            </div>
+            <p className="text-white text-sm">
+              {name}
+            </p>
+          </div>
+          <button 
+            type="button"
+            onClick={() => downloadImage(_id, photo)}
+            className="outline-none bg-transparent border-none"
+          >
+            <img 
+              src={download}
+              alt="download"
+              className="w-6 h-6 object-contain invert"
+            />
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
